9e8d19784f35a39e5f364919073cd9f063088b60
Norange01 <67119887+Norange01@users.noreply.github.com>
2023-02-20 02:57:47 -0500

    Establishes one bno connection

--- fourSensors/fourSensors.ino
+++ fourSensors/fourSensors.ino
@@ -3,10 +3,10 @@
 #include <Adafruit_BNO055.h>
 
 #define BNO055_I2C_ADDR 0x28
-#define I2C_SDA1 32
-#define I2C_SCL1 33
-#define I2C_SDA2 21
-#define I2C_SCL2 22
+#define I2C_SDA2 32
+#define I2C_SCL2 33
+#define I2C_SDA1 21
+#define I2C_SCL1 22
 #define FORCE1 25
 #define FORCE2 26
 TwoWire I2CBNO1 = TwoWire(0);
@@ -14,25 +14,26 @@ TwoWire I2CBNO2 = TwoWire(0);
 
 // Initialize two BNO055 sensors
 //Adafruit_BNO055 bno1 = Adafruit_BNO055(I2CBNO1,BNO055_I2C_ADDR);
-//Adafruit_BNO055 bno2 = Adafruit_BNO055(57);
+Adafruit_BNO055 bno2 = Adafruit_BNO055(55);
 
 //I2CBNO2.begin(I2C_SDA2, I2C_SCL2, 100000);
 
 void setup() {
   Serial.begin(921600);
+  Wire.begin(I2C_SDA2,I2C_SCL2); 
   //I2CBNO1.begin(I2C_SDA1, I2C_SCL1, 100000);
 
   // Initialize BNO055 sensors
   //bno1.begin(0x76, &I2CBNO1); 
-  //status2 = bno2.begin(0x76, &I2CBNO2); 
+ 
   //if (!status1) {
   //  Serial.println("Could not find valid BNO1 sensor, check wiring!");
   //  while (1);
   //}
-  //if (!status2) {
-  //  Serial.println("Could not find valid BNO2 sensor, check wiring!");
-  //  while (1);
-  //}
+  if (!bno2.begin()) {
+    Serial.println("Could not find valid BNO2 sensor, check wiring!");
+    //while (1);
+  }
 
 
 }
@@ -49,20 +50,20 @@ void loop() {
   Serial.print(event1.orientation.y);
   Serial.print(", Z: ");
   Serial.print(event1.orientation.z);
-  Serial.println();
+  Serial.println();*/
 
   // Read data from BNO055 sensor 2
-  //sensors_event_t event2;
-  //bno2.getEvent(&event2);
+  sensors_event_t event2;
+  bno2.getEvent(&event2);
 
-  //Serial.print("BNO055 Sensor 2: ");
-  //Serial.print("Orientation X: ");
-  //Serial.print(event2.orientation.x);
-  //Serial.print(", Y: ");
-  //Serial.print(event2.orientation.y);
-  //Serial.print(", Z: ");
-  //Serial.print(event2.orientation.z);
-  //Serial.println();*/
+  Serial.print("BNO055 Sensor 2: ");
+  Serial.print("Orientation X: ");
+  Serial.print(event2.orientation.x);
+  Serial.print(", Y: ");
+  Serial.print(event2.orientation.y);
+  Serial.print(", Z: ");
+  Serial.print(event2.orientation.z);
+  Serial.println();
 
   // Read data from force sensor 1
   float force2_reading = abs(4095-analogRead(FORCE2));
